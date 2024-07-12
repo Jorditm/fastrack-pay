59441d38f43c68dd43beac27eb8886642fb470b7
jorvixsky <jorvixsky@proton.me>
2024-07-12 23:53:41 +0200

    Added methods to store subscriptions in company contract

--- contracts/contracts/CompanyWallet.sol
+++ contracts/contracts/CompanyWallet.sol
@@ -11,6 +11,9 @@ contract CompanyWallet is Ownable {
     event ProductDisabled(bytes32 productId);
     event ProductEnabled(bytes32 productId);
     event PaymentReceived(address customer, uint256 amount);
+    event CustomerSubscribed(address customer, bytes32 productId);
+    event CustomerUnsubscribed(address customer, bytes32 productId);
+    event CustomerCharged(address customer, bytes32 productId);
 
     struct ProductInfo {
         uint256 price;
@@ -18,10 +21,28 @@ contract CompanyWallet is Ownable {
         string title; 
         string description;
         string imageUrl;
+        bool recurring;
+        uint256 interval;
+    }
+
+    struct CustomerSubscriptionInfo {
+        address customer;
+        bytes32 productId;
+        uint256 lastPayment;
+        uint256 nextPayment;
+        bool isActive;
+    }
+
+    struct CustomersCharged {
+        address customer;
+        bytes32 productId;
     }
 
     bytes32[] public productIds;
     mapping(bytes32 => ProductInfo) public productsInfo;
+    mapping(address => bytes32[]) public customerSubscriptions;
+    mapping(address => mapping(bytes32 => CustomerSubscriptionInfo)) public customerSubscriptionsStatus;
+
 
     constructor(
         address _owner
@@ -79,4 +100,45 @@ contract CompanyWallet is Ownable {
         emit PaymentReceived(_customer, productsInfo[_productId].price);
     }
 
+    function addSubscription(address _customer, bytes32 _productId) external {
+        require(productsInfo[_productId].available, "Product is not available");
+        require(productsInfo[_productId].recurring, "Product is not recurring");
+        customerSubscriptions[_customer].push(_productId);
+        CustomerSubscriptionInfo memory subscription = CustomerSubscriptionInfo({
+            customer: _customer,
+            productId: _productId,
+            lastPayment: block.timestamp,
+            nextPayment: block.timestamp + productsInfo[_productId].interval,
+            isActive: true
+        });
+        customerSubscriptionsStatus[_customer][_productId] = subscription;
+        emit CustomerSubscribed(_customer, _productId);
+    }
+
+    function cancelSubscription(address _customer, bytes32 _productId) external {
+        customerSubscriptionsStatus[_customer][_productId].isActive = false;
+        emit CustomerUnsubscribed(_customer, _productId);
+    }
+
+    function reactivateSubscription(address _customer, bytes32 _productId) external onlyOwner {
+        require(productsInfo[_productId].available, "Product is not available");
+        require(productsInfo[_productId].recurring, "Product is not recurring");
+        require(!customerSubscriptionsStatus[_customer][_productId].isActive, "Subscription is already active");
+        customerSubscriptionsStatus[_customer][_productId].isActive = true;
+        customerSubscriptionsStatus[_customer][_productId].lastPayment = block.timestamp;
+        customerSubscriptionsStatus[_customer][_productId].nextPayment = block.timestamp + productsInfo[_productId].interval;
+        emit CustomerSubscribed(_customer, _productId);
+    }
+
+    function getCustomerSubscriptions(address _customer) external view returns (bytes32[] memory) {
+        bytes32[] memory subscriptions = new bytes32[](customerSubscriptions[_customer].length);
+        for (uint i = 0; i < customerSubscriptions[_customer].length; i++) {
+            subscriptions[i] = customerSubscriptions[_customer][i];
+        }
+        return subscriptions;
+    }
+
+    function getCustomerSubscriptionStatus(address _customer, bytes32 _productId) external view returns (CustomerSubscriptionInfo memory) {
+        return customerSubscriptionsStatus[_customer][_productId];
+    }
 }
