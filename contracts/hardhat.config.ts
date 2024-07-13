5d25115753b8c518aeb6241771c7571bc63f069c
jorvixsky <jorvixsky@proton.me>
2024-07-13 11:11:44 +0200

    Added sepolia testnet

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,6 +2,9 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "hardhat-gas-reporter";
 
+const WALLET_PRIVATE_KEY=process.env.WALLET_PRIVATE_KEY;
+const ALCHEMY_SEPOLIA_URL=process.env.ALCHEMY_SEPOLIA_URL;
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.26",
@@ -16,6 +19,10 @@ const config: HardhatUserConfig = {
     localhost: {
       url: "http://127.0.0.1:8545",
     },
+    sepolia: {
+      url: ALCHEMY_SEPOLIA_URL!,
+      accounts: [WALLET_PRIVATE_KEY!]
+    }
   },
   defaultNetwork: "localhost"
 };
