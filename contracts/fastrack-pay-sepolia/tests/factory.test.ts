ad6ec901aa798429de4d0b2761f18338f99d6c73
jorvixsky <jorvixsky@proton.me>
2024-07-13 22:17:12 +0200

    Initialize subgraph

---
+++ contracts/fastrack-pay-sepolia/tests/factory.test.ts
@@ -0,0 +1,49 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address } from "@graphprotocol/graph-ts"
+import { CompanyAccountCreated } from "../generated/schema"
+import { CompanyAccountCreated as CompanyAccountCreatedEvent } from "../generated/Factory/Factory"
+import { handleCompanyAccountCreated } from "../src/factory"
+import { createCompanyAccountCreatedEvent } from "./factory-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let _contract = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let newCompanyAccountCreatedEvent =
+      createCompanyAccountCreatedEvent(_contract)
+    handleCompanyAccountCreated(newCompanyAccountCreatedEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("CompanyAccountCreated created and stored", () => {
+    assert.entityCount("CompanyAccountCreated", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "CompanyAccountCreated",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "_contract",
+      "0x0000000000000000000000000000000000000001"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
