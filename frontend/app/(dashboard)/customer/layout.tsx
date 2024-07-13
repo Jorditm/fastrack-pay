82ebc3e620eadafcbf3d63dae9e458a340870bc8
Hector Esquerdo Valverde <hector@hevprojects.com>
2024-07-13 19:57:56 +0200

    Dashboard route protection

--- frontend/app/(dashboard)/customer/layout.tsx
+++ frontend/app/(dashboard)/customer/layout.tsx
@@ -1,7 +1,21 @@
+"use client"
+import { useEffect } from 'react'
 import Layout from '@/components/ui/layout'
 import { Sidebar } from '@/components/ui/sidebar'
+import { redirect } from 'next/navigation'
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+    useEffect(() => {
+        const user = localStorage.getItem('user')
+        const companyAddress = localStorage.getItem('companyAddress')
+        if(user){
+            if(!companyAddress){
+                redirect('/company/customers')
+            }
+        }else{
+            redirect('/')
+        }
+    }, [])
 
     return (
         <Layout sidebar={<Sidebar clientType="customer" routes={['payments', 'subscriptions']} />}>
